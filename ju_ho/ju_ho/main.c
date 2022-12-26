07437557b7392c5058e107549060a164e8cc9274
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2022-12-26 16:25:05 +0900

    TEST2

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,5 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int fact(int n) {
+	if (n == 0)
+	{
+		return 1;
+	}
+	if (n == 1)
+	{
+		return 1;
+	}
+	return n * fact(n - 1);
+}
 int main() {
-	printf("»≠¿Ã∆√!");
+	int x;
+	scanf("%d", &x);
+	printf("%d", fact(x));
+	return 0;
 }
