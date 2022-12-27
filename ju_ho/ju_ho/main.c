64745840ecd7ab619b69e68e42548e30214a8800
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2022-12-27 12:39:59 +0900

    2557 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,16 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-	int N = 0;
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < i+1; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
+	printf("Hello World!");
 	return 0;
 }
