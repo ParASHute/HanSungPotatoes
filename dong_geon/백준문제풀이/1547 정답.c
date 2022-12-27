18bcf0165b67c8e51a7b4a645b10be7881f0b9b6
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2022-12-27 14:03:07 +0900

    1547 공 (구현, 포인터)

---
+++ dong_geon/백준문제풀이/1547 정답.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void change(int *x, int *y)  //포인터 주소값으로 값 변경
+{
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+int main(void)
+{
+	int m, x, y, ball = 1;  //공 초기위치 1
+	scanf("%d", &m);
+	while (m--) {
+		scanf("%d %d", &x, &y);
+
+		if (x==ball) { //만약 x 컴이 공이라면 
+			change(&x, &y);  //x와 y의 위치를 바꾸고 x=1,y=2 에서 x=2,y=1 과정
+			ball = x;   //공은 x자리에 있게되는 것. (x=2)
+		}
+		if (y == ball) {
+			change(&x, &y);
+			ball = y;
+		}
+	}
+	printf("%d", ball);
+}
