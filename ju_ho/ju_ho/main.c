ae2d9a6b2a22030e67d04d45d033b866fadeee0d
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2022-12-27 13:25:44 +0900

    1712

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -2,12 +2,19 @@
 #include <stdio.h>
 
 int main() {
-	int A, B = 0;
-	scanf("%d %d", &A, &B);
-	printf("%d\n", A + B);
-	printf("%d\n", A - B);
-	printf("%d\n", A * B);
-	printf("%d\n", A / B);
-	printf("%d\n", A % B);
+	int A, B, C = 0;
+	scanf("%d %d %d", &A, &B, &C);
+	
+	if (B >= C)
+	{
+		printf("-1");
+	}
+	for (int i = 1; i <= A+1; i++)
+	{
+		if (A + B * i == C * i) {
+			printf("%d", i + 1);
+			break;
+		}
+	}
 	return 0;
 }
