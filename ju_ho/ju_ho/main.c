e5acedbb844dd8921645204bf8279b33cf7f901d
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2022-12-28 14:01:52 +0900

    2480 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,18 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
-int main() {
-	int M, X, Y;
-	int cup = 1;
-	scanf("%d", &M);
-	for (int i = 0; i < M; i++)
+int max(int a, int b, int c) {
+	if (a > b && a > c)
 	{
-		scanf("%d %d", &X, &Y);
-		if (X == cup)
-			cup = Y;
-		else if (Y == cup)
-			cup = X;
+		return a;
 	}
-	printf("%d", cup);
+	else if (b > a && b > c)
+	{
+		return b;
+	}
+	else
+		return c;
+}
+int main() {
+	int a, b, c=0;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a == b && b == c && a == c) {
+		printf("%d", 10000 + a * 1000);
+	}
+	else if (a == b || a == c) {
+		printf("%d", 1000 + (a) * 100);
+	}
+	else if (b == c) {
+		printf("%d", 1000 + (b)*100);
+	}
+	else
+		printf("%d", max(a, b, c) * 100);
 	return 0;
 }
