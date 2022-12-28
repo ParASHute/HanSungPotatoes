23c8f660dac2d82e501401b9deb7dbfc3097b5af
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2022-12-28 13:00:30 +0900

    2783 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -2,8 +2,18 @@
 #include <stdio.h>
 
 int main() {
-	int x;
-	scanf("%x", &x);
-	printf("%d", x);
+	int X, Y, N = 0;
+	float div,val = 0;
+	scanf("%d %d", &X, &Y);
+	scanf("%d", &N);
+	val = X * 1000.0 / Y;
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d %d", &X, &Y);
+		div = X * 1000.0 / Y;
+		if (val >= div)
+			val = div;
+	}
+	printf("%0.2f", val);
 	return 0;
 }
