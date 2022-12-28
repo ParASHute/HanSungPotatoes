c6891c9e46416bafab9109ffe0260f8d903d2d0d
jinyoung0312 <abba4639@naver.com>
2022-12-28 12:01:32 +0900

    2557

--- jinyoung/main.c
+++ jinyoung/main.c
@@ -1,15 +1,5 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
 int main() {
-	int N;
-
-	scanf("%d", &N);
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < i+1; j++) {
-			printf("*");
-		}
-		printf("\n");
-	}
+	printf("Hello World!");
 }
