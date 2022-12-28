572917299f366880b69cdc1652bcd0a811584a73
ParASHute <ktwykhs@naver.com>
2022-12-28 23:16:00 +0900

    김민성 백준 1668 트로피 전열 정답

--- algorithm/algorithm/main.c
+++ algorithm/algorithm/main.c
@@ -1,21 +1,8 @@
-/* 김민성 백준 1668 트로피 전열 */
+/* 김민성 백준 1668 트로피 전열 정답 */
 #include <stdio.h>
-
-int main(){
-    
-    int arr[50] = { 0 };
-    int l = 1,r = 1;
-    int num,temp = 0,h;
-    int p,q,k;
-    
-    scanf("%d", &num);
-    k = num;
-    
-    for(int i = 0; i < num; i++){
-        scanf("%d", &h);
-        arr[i] = h;
-    }
-    
+int arr[50];
+int ll(int l, int num){
+    int p,q,temp = 0;
     for(int i = 0; i < num; i++){
         p = arr[i];
         q = arr[i + 1];
@@ -30,9 +17,12 @@ int main(){
                 temp = p;
         }
     }
-    temp = 0;
-    p = 0;
-    q = 0;
+    return l;
+}
+
+int lr(int r, int num){
+    int temp = 0, k = num;
+    int p,q;
     for(int i = 0; i < num; i++){
         p = arr[k - 1];
         q = arr[k - 2];
@@ -50,6 +40,19 @@ int main(){
         if(k-2 < 0)
             break;
     }
+    return r;
+}
+int main(){
     
-    printf("%d\n%d",l,r);
+    int l = 1,r = 1;
+    int num,h;
+    
+    scanf("%d", &num);
+    
+    for(int i = 0; i < num; i++){
+        scanf("%d", &h);
+        arr[i] = h;
+    }
+    
+    printf("%d\n%d",ll(l, num),lr(r,num));
 }
