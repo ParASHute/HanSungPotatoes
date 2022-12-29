9b045f0fbeb269fe03daff18e66fbf589951d6f4
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2022-12-29 22:55:53 +0900

    2783.c정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
+//2783 삼각김밥 정답.c
+
 int main(void)
 {
-	int e, f, c, a = 0, b=0, can = 0;
-	scanf("%d %d %d", &e, &f, &c);
-	a = (e + f) / c;  
-	can += a;
-	b += a;  //새로운 빈 병 b
-	b += (e + f) % c; //빈 병 나머지 있을 수 있음 ex(5,5,3)
-
-	while (1)
-	{
-		if (b / c >= 1) {
-			b -= c;
-			b++;
-			can++;
-		}
-		else
-			break;
+	int i, n;
+	double a[101], b[101], x, y, min = 1000000;
+	scanf("%lf %lf", &x, &y);
+	if (min > x / y * 1000)
+		min = x / y * 1000;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf("%lf %lf", &a[i], &b[i]);
+		if (min > a[i] / b[i] * 1000)
+			min = a[i] / b[i] * 1000;
 	}
-	printf("%d", can);
+	printf("%.2lf",min);
 }
 
+//문제에 쓸데없는 말이 너무 많다. 그냥 그램당 가격 가장 작은애 골라주면 되는 문제.
