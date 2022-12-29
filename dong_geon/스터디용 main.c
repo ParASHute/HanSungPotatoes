d28d6026fc7954e3302a632a17816265ec681d5b
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2022-12-30 00:56:15 +0900

    2980 도로와 신호등 동건 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 
-//2783 삼각김밥 정답.c
+//2980 도로와 신호등, 쉬운 실버문제지만 생각보다 어려운 문제. 조건 하나하나 생각하면서 잘 설정해주어야함
+//핵심은 17~26번째 줄을 구현해내는 것. 나누기 연산자 활용!
 
 int main(void)
 {
-	int i, n;
-	double a[101], b[101], x, y, min = 1000000;
-	scanf("%lf %lf", &x, &y);
-	if (min > x / y * 1000)
-		min = x / y * 1000;
-	scanf("%d", &n);
-	for (i = 0; i < n; i++) {
-		scanf("%lf %lf", &a[i], &b[i]);
-		if (min > a[i] / b[i] * 1000)
-			min = a[i] / b[i] * 1000;
-	}
-	printf("%.2lf",min);
-}
+	int i, n, l, d[101]={0}, r[101], g[101],time, x = 0, t = 0; //x는 거리 t는 시간
+	scanf("%d %d", &n, &l);
+	for (i = 0; i < n; i++) 
+		scanf("%d %d %d", &d[i], &r[i], &g[i]); //인덱스는 i 번째 신호등
 
-//문제에 쓸데없는 말이 너무 많다. 그냥 그램당 가격 가장 작은애 골라주면 되는 문제.
+	for (i = 0; i < n; i++)  //도로 시작부터 마지막 신호등까지
+	{
+		time = r[i] + g[i];
+		while (x != l) {  //신호등을 만나기위한 대충 조건.
+			x++;
+			t++;
+			if (x == d[i])  //현재 거리와 신호등과 만났을 때
+			{
+				if(t%time>=r[i]) 
+					break;
+				else
+				{
+					t += r[i] - (t % time);
+					break;
+				}
+			}
+		}
+	} 
+
+	while (1) // 마지막 신호등 끝나고부터 도로끝까지의 거리 추가
+	{
+		x++;
+		t++;
+		if (x == l) 
+			break;
+	}
+
+	printf("%d", t);
+}
