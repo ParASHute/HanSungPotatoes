507357959eceace76226b8d6e251ae664b61942d
ParASHute <ktwykhs@naver.com>
2022-12-30 13:43:51 +0900

    김민성 백준 2748 dp

--- algorithm/algorithm/main.c
+++ algorithm/algorithm/main.c
@@ -1,13 +1,20 @@
-/* 김민성 백준 2748 재귀(시간초과) */
+/* 김민성 백준 2748 dp */
 #include <stdio.h>
 
+long long dp[90];
 long long Fibonacci(long long n){
     
-    if ( n <= 1 ) {
-        return n;
-      }
-      return Fibonacci(n-1) + Fibonacci(n-2);
-    }
+    if (n == 1 || n == 2)
+        return 1;
+    
+    if(dp[n] != 0)
+      return dp[n];
+    
+    else
+        dp[n] = Fibonacci(n-1) + Fibonacci(n-2);
+    
+    return dp[n];
+}
 
 int main(){
     
@@ -16,18 +23,3 @@ int main(){
     scanf("%lld",&count);
     printf("%lld", Fibonacci(count));
 }
-
-
-//이문제 말 구하는건 안어렵고 입력 받는게 살짝 까다로웠던 것 같았는데 효율적인 방법이네요 
-//저는 이렇게했어요
-
-/*
-for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-			scanf(" %c", &arr[i][j]); 
-	}
-*/
-
-//이런식으로 8x8 칸에 넣어줘도 제대로 들어가더라구요. 특징은 %c 앞에 공백이 들어가는건데
-//이거에 대해서 같이 함 공부해봐도 유용할듯 합니다. 저도 아직 헷갈리구요,
