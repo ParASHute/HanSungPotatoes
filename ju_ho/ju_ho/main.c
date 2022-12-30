79832806e39b0d88be92b1a37047328fec1c46fc
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2022-12-30 12:39:01 +0900

    2752 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 
 int main() {
-	int N = 0;
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = N; j > i ; j--)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a < b && b < c)
+		printf("%d %d %d", a, b, c);
+	else if (b < a && a < c)
+		printf("%d %d %d", b, a, c);
+	else if (a < c && c < b)
+		printf("%d %d %d", a, c, b);
+	else if (b<c&&c<a)
+		printf("%d %d %d", b, c, a);
+	else if (c < a && a < b)
+		printf("%d %d %d", c, a, b);
+	else if (c < b && b < a)
+		printf("%d %d %d", c, b, a);
 	return 0;
 }
