8d52288953f9d38a3f2de75b286e406d3561ccf2
jinyoung0312 <121417175+jinyoung0312@users.noreply.github.com>
2022-12-30 12:53:15 +0900

    2752번

--- jinyoung/main.c
+++ jinyoung/main.c
@@ -2,17 +2,11 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, n, sum;
-	scanf("%d", &n);
-
-	for (int j = 0; j < n; j++) {
-		sum = 1;
-		scanf("%d %d", &a, &b);
-		for (int i = 0; i < b; i++) {
-			sum *= a;
-			sum %= 10;
-		}
-		if (sum == 0) printf("10\n");
-		else printf("%d\n", sum);
-	}
+	int a, b, c, d;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a > b) { d = a; a = b; b = d; }
+	if (b > c) { d = b; b = c; c = d; }
+	if (a > b) { d = a; a = b; b = d; }
+	if (a > c) { d = a; a = c; c = d; }
+	printf("%d %d %d", a, b, c);
 }
