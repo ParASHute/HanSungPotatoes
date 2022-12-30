419025dab7686a942e1bcd23999f41c43f6bcc61
jinyoung0312 <121417175+jinyoung0312@users.noreply.github.com>
2022-12-30 12:51:04 +0900

    1712번

--- jinyoung/main.c
+++ jinyoung/main.c
@@ -2,7 +2,13 @@
 #include <stdio.h>
 
 int main() {
-	int x;
-	scanf("%x", &x);
-	printf("%d", x);
+	int a, b, c, sum1, sum2 = 0, n = 0;
+	scanf("%d %d %d", &a, &b, &c);
+
+	sum1 = a;
+	if (b >= c) printf("-1\n");
+	else {
+		n = a / (c - b) + 1;
+		printf("%d", n);
+	}
 }
