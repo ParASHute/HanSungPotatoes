ac0a3dd8832ea1840651501576d46c5197b6eedf
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2022-12-31 01:25:16 +0900

    2721삼각수의 합

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,38 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int main(void)
+//2721 삼각수의 합 정답.c
+int main(void) 
 {
-	char s[55];
-	int i, j, len, sum = 0;
-	scanf("%s", s);
-	len = strlen(s);
-	for (i = 0; i < len; i++)
-	{
-		if (s[i] == '-')
+	int n, a, sum, i, plus;
+	scanf("%d", &n);
+	while (n--) {
+		sum = 0;
+		scanf("%d", &a);
+		for (i = 1; i <= a; i++)
 		{
-			j = i;
-			while (1)
-			{
-				if (j + 2 >= len)
-					break;
+			plus = 0;
+			for (int j = 1; j <= i + 1; j++)
+				plus += j;
+			sum += plus * i;
 
-				if (s[j + 1] == '+')
-					s[j + 1] = '-';
-
-				if (s[j + 2] == '-')
-					break;
-
-				j++;
-			}
 		}
-	}
-	for (i = 0; i < len; i++)  //55-50-40
-	{
-		printf("%c", s[i]);
+		printf("%d\n", sum);
 	}
 }
-
-//str2int ->str[0]*10 + str[1]*10^1 + ... srt[n-1]*10^n
-//	-> 짜르는 부분으로 n을 초기화 -> inte[] -> 배열에 입력
