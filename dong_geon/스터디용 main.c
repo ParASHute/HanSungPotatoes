163c0d9c4d5bb0f60d2dfe556193607e3c472aa5
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2022-12-31 19:12:23 +0900

    10987 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,43 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
-int gcd(int x, int y) //유클리드 호제로 최대공약수 빠르게
-{
-	if (y == 0)
-		return x;
-	else
-		return gcd(y, x % y);
-}
 
 int main(void) 
 {
-	int a, b, c, d, i, g;   //  a/b + c/d
-	int x, y;  // 기약분수 y/x
-	int abgcd, cdgcd;
-	scanf("%d %d", &a, &b);
-	scanf("%d %d", &c, &d);
-	abgcd = gcd(a, b);
-	cdgcd = gcd(c, d);
-	if (abgcd != 1) {  //약분
-		a /= abgcd;
-		b /= abgcd;
+	char arr[101];
+	int n = 0, i;
+	scanf("%s", arr);
+	for (i = 0; i < strlen(arr); i++)
+	{
+		if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u')
+			n++;
 	}
-	if (cdgcd != 1) {  //약분
-		c /= cdgcd;
-		d /= cdgcd;
-	}
-	
-	x = b * d;   //분모
-	y = a * d + b * c;   //분자
-
-	while (1) {
-		g = gcd(x, y);
-
-		if (gcd(x, y) == 1) {  //최대공약수가 1이면 기약분수인 특징를
-			printf("%d %d", y, x);
-			return 0;
-		}
-
-		x /= g;
-		y /= g;
-	}
+	printf("%d", n);
 }
