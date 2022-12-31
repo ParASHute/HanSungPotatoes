00a924c549971a578e5ded4d2ae8ce3aa88ee9ff
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-01 05:41:23 +0900

    27065 2022년이 아름다웠던 이유,정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -2,20 +2,43 @@
 
 int main(void) 
 {
-	int n, i, count;
-	long long x;
-	while (scanf("%d", &n) != EOF) {
-		x = 1;
+	int t, n, i, sum, sum2, count,check, num[500];
+	scanf("%d", &t);
+	while (t--) {
+		scanf("%d", &n);
+		sum = 0;
 		count = 0;
-		while (1) {
-			count++;
-			if (x % n == 0) {
-				printf("%d\n", count);
-				break;
+		check = 0;
+		for (i = 2; i < n; i++)
+		{
+			if (n % i == 0) {
+				sum += i;
+				num[count] = i;
+				count++;
 			}
-			x *= 10;
-			x += 1;
-			x %= n;
 		}
+		if (sum > n)
+		{
+			for (i = 0; i < count; i++)
+			{
+				sum2 = 0;
+				for (int j = 2; j < num[i]; j++)
+				{
+					if (num[i] % j == 0)
+						sum2 += j;
+				}
+				if (sum2 > num[i]) {
+					printf("BOJ 2022\n");
+					break;
+				}
+				check++;
+			}
+			if (check==count)
+				printf("Good Bye\n");
+		}
+
+		else
+			printf("BOJ 2022\n");
 	}
+	return 0;
 }
