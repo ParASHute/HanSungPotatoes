66b41f1412dd47d68b12e8bdf9c4a3e1510ca2b6
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-01-01 21:57:30 +0900

    10101 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,13 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
-int main() { int a, b, c, d, A, B, C, D; 
-	scanf("%d %d %d %d", &a, &b, &c, &d); 
-	scanf("%d %d %d %d", &A, &B, &C, &D); 
-	if (a + b + c + d >= A + B + C + D) { 
-		printf("%d", a + b + c + d); 
-	} 
-	else { 
-		printf("%d", A + B + C + D); 
-	} 
-	return 0; 
+
+int main() {
+
+	int a, b, c;
+
+	scanf("%d", &a);
+
+	scanf("%d", &b);
+
+	scanf("%d", &c);
+
+	if (a == 60 && b == 60 && c == 60) { printf("Equilateral"); }
+
+	else if (a + b + c == 180 && a == b || a == c || b == c) {
+
+		printf("Isosceles");
+	}
+
+	else if (a + b + c == 180 && a != b && a != c && b != c) {
+
+		printf("Scalene");
+
+	}
+	else if (a + b + c != 180) {
+
+		printf("Error");
+	}
+
+	return 0;
 }
