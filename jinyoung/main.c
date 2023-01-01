93eaf5b447465eeacbbf63aa976605133506da6e
jinyoung0312 <121417175+jinyoung0312@users.noreply.github.com>
2023-01-01 13:18:29 +0900

    2442번

--- jinyoung/main.c
+++ jinyoung/main.c
@@ -3,10 +3,18 @@
 
 int main() {
 	int n;
+
 	scanf("%d", &n);
 
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n - i; j++) {
+		for (int j = 1; j < n - i; j++) {
+			printf(" ");
+		}
+		for (int k = 0; k < i; k++) {
+			printf("*");
+		}
+		printf("*");
+		for (int t = 0; t < i; t++) {
 			printf("*");
 		}
 		printf("\n");
