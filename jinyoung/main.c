2d2128223617a5e9a77ca272fa904380cfe23079
jinyoung0312 <121417175+jinyoung0312@users.noreply.github.com>
2023-01-01 13:17:34 +0900

    2439번

--- jinyoung/main.c
+++ jinyoung/main.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, c, sum;
-	scanf("%d", &a);
-	scanf("%d", &b);
-	scanf("%d", &c);
-	sum = a + b + c;
-	if (sum != 180) printf("Error");
-	else if (a == b && b == c && a == c)printf("Equilateral");
-	else if (a != b && b != c && c != a)printf("Scalene");
-	else printf("Isosceles");
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 1; j < n - i; j++) {
+			printf(" ");
+		}
+		for (int k = 0; k < i; k++) {
+			printf("*");
+		}
+		printf("*\n");
+	}
 }
