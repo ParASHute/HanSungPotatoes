bec2ba4c077741c7e6bb9665ff421301aaafc242
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-01-01 22:51:06 +0900

    2442 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,17 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 
 int main() {
-	int A, B, C;
-
-	scanf("%d %d %d", &A, &B, &C);
-
-	printf("%d\n", (A + B) % C);
-
-	printf("%d\n", ((A % C) + (B % C)) % C); printf("%d\n", (A * B) % C);
-
-	printf("%d\n", ((A % C) * (B % C)) % C);
-
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+	{
+		for (int k = N-1; k > i; k--)
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < 2*i+1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 	return 0;
 }
