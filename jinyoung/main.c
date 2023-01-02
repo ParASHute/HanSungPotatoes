d6d01097ba9684da1790e341529f954ef4045e63
jinyoung0312 <121417175+jinyoung0312@users.noreply.github.com>
2023-01-02 13:30:33 +0900

    2490번

--- jinyoung/main.c
+++ jinyoung/main.c
@@ -2,28 +2,14 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, c, d, sum, n, max = 0;
-	scanf("%d", &n);
-
-	for (int i = 0; i < n; i++) {
-		scanf("%d %d %d", &a, &b, &c);
-
-		if (a <= b) { d = a; a = b; b = d; }
-		if (b <= c) { d = b; b = c; c = d; }
-		if (a <= b) { d = a; a = b; b = d; }
-		if (a <= c) { d = a; a = c; c = d; }
-
-		if (a == b && a == c && b == c) {
-			sum = 10000 + a * 1000;
-		}
-		else if (a != b && a != c && b != c) {
-			sum = a * 100;
-		}
-		else {
-			sum = 1000 + b * 100;
-		}
-		if (max < sum)max = sum;
+	int a, b, c, d, sum;
+	for (int i = 0; i < 3; i++) {
+		scanf("%d %d %d %d", &a, &b, &c, &d);
+		sum = a + b + c + d;
+		if (sum == 0) printf("D\n");
+		else if (sum == 1) printf("C\n");
+		else if (sum == 2) printf("B\n");
+		else if (sum == 3) printf("A\n");
+		else if (sum == 4) printf("E\n");
 	}
-
-	printf("%d", max);
 }
