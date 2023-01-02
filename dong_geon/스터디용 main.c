24ab9d27686a70990785aeb5c1232d54a6b9c3fd
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-03 00:33:29 +0900

    11004 K번째 수 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int arr[100001];
+int arr[5000001];
 int static compare(const void* first, const void* second)
 {
     if (*(int*)first > *(int*)second)
@@ -14,15 +14,10 @@ int static compare(const void* first, const void* second)
 
 int main(void)
 {
-    int n, i;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-    qsort(arr, n, 4, compare);
-    printf("%d ", arr[0]);
-    for (i = 1; i < n; i++) {
-        if(arr[i-1]!=arr[i])
-            printf("%d ", arr[i]);
-    }
+	int n, k;
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+	qsort(arr, n, 4, compare);
+    printf("%d", arr[k - 1]);
 }
-
