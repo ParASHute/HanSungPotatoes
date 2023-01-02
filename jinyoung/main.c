749a3891fa01cac0e05f0c649e69b76a87e64699
jinyoung0312 <121417175+jinyoung0312@users.noreply.github.com>
2023-01-02 13:29:33 +0900

    2163번

--- jinyoung/main.c
+++ jinyoung/main.c
@@ -2,30 +2,9 @@
 #include <stdio.h>
 
 int main() {
-	int n, max = 0, a = 0;
-	int t[50] = { 0 };
+	int n, m;
 
-	scanf("%d", &n);
+	scanf("%d %d", &n, &m);
 
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &t[i]);
-	}
-
-	for (int i = 0; i < n; i++) {
-		if (max < t[i]) {
-			max = t[i];
-			a++;
-		}
-	}
-
-	printf("%d\n", a);
-
-	a = 0; max = 0;
-	for (int i = n - 1; i >= 0; i--) {
-		if (max < t[i]) {
-			max = t[i];
-			a++;
-		}
-	}
-	printf("%d\n", a);
+	printf("%d", n * m - 1);
 }
