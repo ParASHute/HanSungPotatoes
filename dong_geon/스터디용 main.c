b204a99562d1f473349f91a61b19eaa4eb3e4f48
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-02 12:41:03 +0900

    1526 금민수 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
-	int a, b, c, n, money[1001] = { 0 }, max = 0;
+	int n, m, i, check;
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		max = 0;
-		scanf("%d %d %d", &a, &b, &c);
-		max = a > b ? a : b;
-		max = max > c ? max : c;
-			
-		if (a == b && b == c)
-			money[i] = 10000 + (a * 1000);
-		else if (a == b || a == c || b == c)
-		{
-			if (a == b)
-				money[i] = 1000 + (a * 100);
-			if (a == c)
-				money[i] = 1000 + (a * 100);
-			if (c == b)
-				money[i] = 1000 + (b * 100);
+	
+	while (1)
+	{
+		check = 0;
+		m = n;
+		while (m != 0) {  // 정수에 7혹은 4가 들어있는지 1의 자리 수 부터 검사
+			if (m % 10 != 7 && m % 10 != 4) {  
+				check = 0;
+				break;
+			}
+			else
+				check = 1;
+			m /= 10;
+		}
+		if (check == 1) {
+			printf("%d", n);
+			break;
 		}
 		else
-			money[i] = max * 100;
+			n--;
 	}
-	max = 0;
-	for (int i = 0; i < n; i++)
-	{
-		if (max < money[i])
-			max = money[i];
-	}
-	printf("%d", max);
 }
