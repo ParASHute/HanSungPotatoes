06ad71c5a6d95868cd2caf3273da4cd1d5a12d64
jinyoung0312 <121417175+jinyoung0312@users.noreply.github.com>
2023-01-02 13:30:59 +0900

    2506번

--- jinyoung/main.c
+++ jinyoung/main.c
@@ -2,14 +2,21 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, c, d, sum;
-	for (int i = 0; i < 3; i++) {
-		scanf("%d %d %d %d", &a, &b, &c, &d);
-		sum = a + b + c + d;
-		if (sum == 0) printf("D\n");
-		else if (sum == 1) printf("C\n");
-		else if (sum == 2) printf("B\n");
-		else if (sum == 3) printf("A\n");
-		else if (sum == 4) printf("E\n");
+	int n, sum = 0, a = 1;
+	int s[100] = { 0 };
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &s[i]);
 	}
+	for (int i = 0; i < n; i++) {
+		if (s[i] == 1) sum += a;
+		if (s[i] == 1 && s[i + 1] == 1) {
+			a++;
+		}
+		if (s[i] == 0)a = 1;
+	}
+
+	printf("%d", sum);
 }
