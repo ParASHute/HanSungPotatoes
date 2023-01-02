f2e23d1893e69de1abf79c57020854ad08738c8e
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-01-02 13:35:58 +0900

    2476 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -2,10 +2,37 @@
 #include <stdio.h>
 
 int main() {
-	int N, M;
-	int sum = 0;
-	scanf("%d %d", &N, &M);
-	sum = N - 1 + (M-1)*(N);
-	printf("%d", sum);
+	int N = 0;
+	int a, b, c = 0;
+	int val[1000] = { 0 };
+	int max = 0;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d %d %d", &a, &b, &c);
+		if (a == b && b == c && a == c) {
+			val[i] = 10000 + a * 1000;
+		}
+		else if (a == b || a == c) {
+			val[i] =  1000 + a * 100;
+		}
+		else if (b == c) {
+			val[i] = 1000 + b * 100;
+		}
+		else if (a > b && a > c) {
+			val[i] = a * 100;
+		}
+		else if (b > a && b > c) {
+			val[i] = b * 100;
+		}
+		else 
+			val[i] = c * 100;
+		
+		if (max < val[i])
+		{
+			max = val[i];
+		}
+	}
+	printf("%d", max);
 	return 0;
 }
