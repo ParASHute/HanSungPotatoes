4ffc56cafd2fdaab377ab65fcce336b8cbec7a1e
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-01-02 13:08:28 +0900

    2163 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -2,34 +2,10 @@
 #include <stdio.h>
 
 int main() {
-	int N,a;
-	int count1 = 0,count2 = 0;
-	int val[100] = { 0 };
-	int max = 0;
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++)
-	{
-		scanf("%d", &a);
-		val[i] = a;
-	}
-	for (int j = 0; j < N; j++)
-	{
-		if (max < val[j])
-		{
-			max = val[j];
-			count1++;
-		}
-	}
-	max = 0;
-	for (int k = N-1; k >= 0; k--)
-	{
-		if (max < val[k])
-		{
-			max = val[k];
-			count2++;
-		}
-	}
-	printf("%d\n", count1);
-	printf("%d", count2);
+	int N, M;
+	int sum = 0;
+	scanf("%d %d", &N, &M);
+	sum = N - 1 + (M-1)*(N);
+	printf("%d", sum);
 	return 0;
 }
