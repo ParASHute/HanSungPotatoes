1c487551c772e9183b0e16719a7822e158caf332
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-02 11:44:52 +0900

    2476 주사위 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(void)
 {
-	int n, i, count = 0, x = 1;
+	int a, b, c, n, money[1001] = { 0 }, max = 0;
 	scanf("%d", &n);
-	if (n < 5)
-	{
-		printf("0");
-		return 0;
-	}
-	else {
-		for (i = 5; i <= n; i++)
+	for (int i = 0; i < n; i++) {
+		max = 0;
+		scanf("%d %d %d", &a, &b, &c);
+		max = a > b ? a : b;
+		max = max > c ? max : c;
+			
+		if (a == b && b == c)
+			money[i] = 10000 + (a * 1000);
+		else if (a == b || a == c || b == c)
 		{
-			if (i % 5 == 0)
-				count++;
-			if (i % 25 == 0)
-				count++;
-			if (i % 125 == 0)
-				count++;
+			if (a == b)
+				money[i] = 1000 + (a * 100);
+			if (a == c)
+				money[i] = 1000 + (a * 100);
+			if (c == b)
+				money[i] = 1000 + (b * 100);
 		}
+		else
+			money[i] = max * 100;
 	}
-	printf("%d", count);
+	max = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (max < money[i])
+			max = money[i];
+	}
+	printf("%d", max);
 }
