cd911d75a60fcabeb9ef98344eae2f47b84e7519
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-01-04 21:16:34 +0900

    1373 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,23 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#define max 1000000
 
-char str[max];
-char a[max];
+char str[1000000];
+char a[333337];
 
 int main() {
 	int j = 0;
 	int sum = 0, t = 1;
-	scanf("%s", &str);
-	if (str[0] == '0' && str[1] == '\0')
-	{
-		printf("0");
-	}
-	if (str[0] == '0' && str[1] == '0' && str[2] == '\0')
-	{
-		printf("0");
-	}
+	scanf("%s", str);
 	int k = strlen(str) - 1;
 		for (int i = 0; i < strlen(str); i++)
 		{
@@ -27,28 +18,23 @@ int main() {
 			if (str[k] == '0')
 				sum += 0;
 			t *= 2;
+			k--;
 			if (t == 8) {
-				if (sum == 0)
-				{
-					a[j] = 48;
-				}
-				else
-					a[j] = 48 + sum;
+				a[j] = 48 + sum;
 				t = 1;
 				sum = 0;
 				j++;
 			}
-			k--;
 			if (k == -1)
 			{
 				a[j] = sum + 48;
 			}
 		}
-		int size = strlen(a) - 1;
+		int size = (strlen(str)-1)/3;
 		while (1) {
 			printf("%c", a[size]);
 			size--;
-			if (size < -1)
+			if (size < 0)
 			{
 				break;
 			}
