fad1b7c531f2c164b661c939a9df2ea91e6cd4d1
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-06 15:41:46 +0900

    11399 ATM 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int count[10001] = { 0 };
-int main(void)
+int static compare(const void* first, const void* second)
 {
-	int n, x, i, max = 0, min = 10001;
-	scanf("%d", &n);
-	for (i = 0; i < n; i++)
-	{
-		scanf("%d", &x);
-		count[x]++;
-	}
-	
-	for (i = 1; i < 10001; i++) {
-		for(int j=0; j<count[i]; j++)
-			printf("%d\n", i);
-	}
+    if (*(int*)first > *(int*)second)
+        return 1;
+    else if (*(int*)first < *(int*)second)
+        return -1;
+    else
+        return 0;
 }
 
+
+int main(void)
+{
+    int n, i, time[1001], sum = 0;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &time[i]);
+	qsort(time, n, 4, compare);
+    
+    sum += time[0];
+    for (i = 1; i < n; i++)
+    {
+        time[i] += time[i - 1];
+
+        sum += time[i];
+    }
+    printf("%d", sum);
+}
