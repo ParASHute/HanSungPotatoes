16dab3361836a3f43b24a405006e24808fa10863
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-07 17:30:21 +0900

    2857 FBI 오답코드 반례 못찾겠음

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
-#include <string.h> //so easy 한 문제
+#include <string.h>
+#include <stdlib.h>
+
 
 int main(void)
 {
-	int i, index = 0;
-	char s[101], name[101] = { 0 };
-	scanf("%s", s);
-	for (i = 0; i < strlen(s); i++)
-	{
-		if (s[i] >= 'A' && s[i] <= 'Z'){ //그냥 대문자면 출력..
-			printf("%c", s[i]);
+	int i, j, len, n = 5, cnt = 0;
+	char arr[6][14];
+	int check[1001];  //check는 요원 숫자 저장
+
+	for (i = 1; i < 6; i++) {
+		
+		scanf("%s", arr[i]);
+		len = strlen(arr[i]);
+
+		for (j = 0; j < len - 2; j++)
+		{
+			if (arr[i][j] == 'F') {
+				if (arr[i][j + 1] == 'B') {
+					if (arr[i][j + 2] == 'I') {
+						check[cnt] = i;
+						cnt++;  //0인지 아닌지 확인용, 위의 check의 인덱스로도 사용
+						break;
+					}
+				}
+			}
 		}
 	}
-}
+
+	if (cnt == 0)
+		printf("HE GOT AWAY!");
+
+	else
+	{
+		for (i = 0; i < 1001; i++)
+		{
+			if (check[i] > 0)
+				printf("%d ", check[i]);
+		}
+	}
+}		
