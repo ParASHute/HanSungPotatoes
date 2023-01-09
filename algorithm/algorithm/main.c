97522a167f508f1eb747bafa51d5f98a54584642
ParASHute <ktwykhs@naver.com>
2023-01-09 18:15:46 +0900

    백준 3052 정답(... 정답을 맞췄는데... 안올리고.... 이상한거만 계속 올리다.... 결국 반례 잡아줘서 성공.....)

--- algorithm/algorithm/main.c
+++ algorithm/algorithm/main.c
@@ -1,10 +1,10 @@
-/* 백준 3052 */
+/* 백준 3052 정답*/
 #include <stdio.h>
 int main(){
-    int n[10] = {'\0'};
-    int a, k, l = 0, i = 0;
-    while(i < 10){
-        int count = 0;
+    int n[10] = {0};
+    int a, k, l = 0;
+    int count = 0;
+    for(int i = 0; i < 10; i++){
         scanf("%d",&a);
         k = a % 42;
         for(int j = 0; j < l; j++){
@@ -12,11 +12,11 @@ int main(){
                 count++;
             }
         }
-        if(count == i){
+        if(count == l){
             n[l] = k;
             l++;
         }
-        i++;
+        count = 0;
     }
     printf("%d\n",l);
 }
