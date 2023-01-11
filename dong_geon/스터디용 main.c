48aaf7d5c0f140c072558156334839a7bb1b1521
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-12 07:44:51 +0900

    9295 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,27 +1,13 @@
-
 #include <stdio.h>
-
 int main(void)
 {
-	int i, j, k, n[16];
-	while (1) {
-		int count = 0;
-		for (i = 0; ; i++)
-		{
-			scanf("%d", &n[i]);
-			if (n[0] == -1)
-				return 0;
-			if (n[i] == 0)
-				break;
-		}
-		for (j = 0; j < i; j++)
-		{
-			for (k = 0; k < i; k++)
-			{
-				if (n[j] * 2 == n[k])
-					count++;
-			}
-		}
-		printf("%d\n", count);
+	int T, a, b;
+	scanf("%d", &T);
+	for(int i=1; i<=T; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("Case %d: %d\n",i, a + b);
 	}
+	return 0;
+}}
 }
