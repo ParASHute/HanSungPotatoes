fe162c3053cd14b05e59bc7b8846f9497179466b
ParASHute <ktwykhs@naver.com>
2023-01-11 12:06:15 +0900

    2577 정답

--- algorithm/algorithm/main.c
+++ algorithm/algorithm/main.c
@@ -1,10 +1,28 @@
-/* 백준 2884 정답*/
+/* 백준 2577 정답*/
 #include <stdio.h>
 int main(){
-    int sH,sM;
-    int m;
-    scanf("%d %d",&sH,&sM);
-    if(sH == 0) sH = 24;
-    m = ((sH * 60) + sM) - 45;
-    (m/60) == 24 ? printf("0 %d", (m % 60)) : printf("%d %d", (m/60), (m % 60));
+    int arr[9];
+    int a,t = 1, k;
+    int count = 0,count1 = 0;
+    for(int i = 0; i < 3; i++){
+        scanf("%d",&a);
+        t *= a;
+    }
+    for(int i = 0; i < 9; i++){
+        k = t % 10;
+        arr[i] = k;
+        t /= 10;
+        if(t != 0){
+            count1++;
+        }
+    }
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < count1+1; j++){
+            if(arr[j] == i){
+                count++;
+            }
+        }
+        printf("%d\n",count);
+        count = 0;
+    }
 }
