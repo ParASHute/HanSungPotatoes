009a4dd51668feaf6e0b6cb9ec1f82f5237960d4
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-13 15:40:46 +0900

    16395 파스칼의 삼각형 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -3,48 +3,19 @@
 
 int main(void)
 {
-	double a, b, c;  //c가 가장 긴 변
-	int n = 1;
-	while (1) {
-		scanf("%lf %lf %lf", &a, &b, &c);
-		if (a == 0 && b == 0 && c == 0)
-			break;
-		if (a < 0) {
-			a = sqrt(pow(c, 2.0) - pow(b, 2.0));
-			if (c < a + b) {
-				printf("Triangle #%d\n", n);
-				printf("a = %.3lf\n\n",a);
-			}
-			else {
-				printf("Triangle #%d\n", n);
-				printf("Impossible.\n\n");
-			}
-		}
-		if (b < 0) {
-			b = sqrt(pow(c, 2.0) - pow(a, 2.0));
-			if (c < a + b) {
-				printf("Triangle #%d\n", n);
-				printf("b = %.3lf\n\n", b);
-			}
-			else {
-				printf("Triangle #%d\n", n);
-				printf("Impossible.\n\n");
-			}
-		}
-		if (c < 0) {
-			c = sqrt(pow(a, 2.0) + pow(b, 2.0));
-			if (c < a + b) {
-				printf("Triangle #%d\n", n);
-				printf("c = %.3lf\n\n", c);
-			}
-			else {
-				printf("Triangle #%d\n", n);
-				printf("Impossible.\n\n");
-			}
-		}
-		
-		n++;
+	double n, i, k, c = 0, pas = 1;
+	scanf("%lf %lf", &n, &k);
 
+	k -= 1;
+	n -= 1;
+	if (k == 0)
+		printf("1");
+	else {
+		for (i = 0; i < k; i++)
+		{
+			pas *= ((n - c) / (i + 1.0));
+			c += 1.0;
+		}
+		printf("%.0lf", pas);
 	}
-
 }
