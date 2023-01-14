3fed36ec568715ce48077f72e47771987a26c09a
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-14 18:55:51 +0900

    2740 행렬의곱셈

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -2,22 +2,31 @@
 
 int main(void)
 {
-	int n, i, j, sum = 0, count = 0;
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
+	int i, j, u, n, m, k;
+	int A[101][101] = { 0 }, B[101][101] = { 0 };
+	scanf("%d %d", &n, &m);
+	for (i = 0; i < n; i++)
 	{
-		sum = 0;
-		for (j = i; j <= n; j++)
-		{
-			sum += j;
-			if (sum == n) {
-				count++;
-				break;
-			}
-
-			if (sum > n)
-				break;
-		}
+		for (j = 0; j < m; j++)
+			scanf("%d", &A[i][j]);
 	}
-	printf("%d", count);
+	scanf("%d %d", &m, &k);
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < k; j++)
+			scanf("%d", &B[i][j]);
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < k; j++)
+		{
+			int sum = 0;
+			for (u = 0; u < m; u++)
+			{
+				sum += A[i][u] * B[u][j];
+			}
+			printf("%d ", sum);
+		}
+		printf("\n");
+	}
 }
