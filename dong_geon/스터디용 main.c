94bc769b872f6732a766b67f376a94012c916327
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-14 16:24:38 +0900

    1312 반례좀 찾아주실 분

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
-#include <math.h>
+
+int arr[1000005] = { 0 }; //소수점 아래 넣어둘 배열
 
 int main(void)
 {
-	double n, i, k, c = 0, pas = 1;
-	scanf("%lf %lf", &n, &k);
+	int a, b, n, ten = 10;
+	int i, same = 0;
+	scanf("%d %d %d", &a, &b, &n);
+	for (i = 1; i <= n; i++)
+	{
+		arr[i] = a * ten / b % 10;  //i번째 소수점 정수로 구하는 식
 
-	k -= 1;
-	n -= 1;
-	if (k == 0)
-		printf("1");
-	else {
-		for (i = 0; i < k; i++)
+		for (int j = 1; j < i; j++)
 		{
-			pas *= ((n - c) / (i + 1.0));
-			c += 1.0;
+			if (arr[i] == arr[j]) {  //소수점 아래 같은 숫자가 나왔을 때
+				same = i;
+			}
 		}
-		printf("%.0lf", pas);
+		if (i == same)  //소수점 아래 같은 숫자가 나왔을 때 탈출
+			break;
+
+		ten *= 10;
+	}
+
+	if (same != 0) {  
+		if (arr[same - 1] != arr[same])  //ex) 25/7 =3.571428 ,571428 ,571 ..
+			printf("%d", arr[n % same + 1]);
+		else  //ex) 10/12 = 0.8333333 or 3/4=0.750000000
+			printf("%d", arr[same]);
+	}
+
+	else {  //n이 같은 숫자 나오기 전보다 작을 때
+		printf("%d", arr[n]);
 	}
 }
