9a3dfb0dbb91b78f2e3bc9bca10d224bbc93621a
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-14 16:41:20 +0900

    1312 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,36 +1,16 @@
-#include <stdio.h>
-
-int arr[1000005] = { 0 }; //소수점 아래 넣어둘 배열
+#include <stdio.h>  //오늘의 교훈 쓸데없이 배열을 사용하지말고 간단히 생각하자..
 
 int main(void)
 {
-	int a, b, n, ten = 10;
-	int i, same = 0;
+	int a, b, n, x;
 	scanf("%d %d %d", &a, &b, &n);
-	for (i = 1; i <= n; i++)
+
+	for (int i = 0; i < n; i++)
 	{
-		arr[i] = a * ten / b % 10;  //i번째 소수점 정수로 구하는 식
-
-		for (int j = 1; j < i; j++)
-		{
-			if (arr[i] == arr[j]) {  //소수점 아래 같은 숫자가 나왔을 때
-				same = i;
-			}
-		}
-		if (i == same)  //소수점 아래 같은 숫자가 나왔을 때 탈출
-			break;
-
-		ten *= 10;
+		a %= b;
+		a *= 10;
+		x = a / b;	
 	}
-
-	if (same != 0) {  
-		if (arr[same - 1] != arr[same])  //ex) 25/7 =3.571428 ,571428 ,571 ..
-			printf("%d", arr[n % same + 1]);
-		else  //ex) 10/12 = 0.8333333 or 3/4=0.750000000
-			printf("%d", arr[same]);
-	}
-
-	else {  //n이 같은 숫자 나오기 전보다 작을 때
-		printf("%d", arr[n]);
-	}
+	printf("%d", x);
+	return 0;
 }
