6d5dec6fce0c4e4ebd5afaeaca4b830ab2f0a280
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-01-16 12:57:03 +0900

    1010 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,35 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-	int N, M;
-	int max = 0;
-	int max1 = 0;
-	int val[102] = { 0 };
-	scanf("%d %d", &N, &M);
-	for (int i = 0; i < N; i++)
+double fact(int a) {
+	if (a <= 1)
 	{
-		scanf("%d", &val[i]);
+		return 1;
 	}
-	
-	for (int j = 0; j < N-2; j++)
+	else
+		return a * fact(a - 1);
+}
+
+int main() {
+	int T, N, M;
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++)
 	{
-		for (int k = j+1; k < N; k++)
-		{
-			for (int q = k+1; q < N; q++)
-			{
-				max = val[j] + val[k] + val[q];
-				if (max <= M)
-				{
-					if (max1 < max)
-					{
-						max1 = max;
-					}
-				}
-				max = 0;
-			}
-		}
-	}	
-	printf("%d", max1);
+		scanf("%d %d", &N, &M);
+		printf("%.0f\n", fact(M) / (fact(M - N) * fact(N)));
+	}
 	return 0;
 }
