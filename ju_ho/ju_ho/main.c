e0aa4aae24b27fbb9e66442a6e9790856fc721db
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-01-16 12:56:08 +0900

    2798 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -2,35 +2,34 @@
 #include <stdio.h>
 
 int main() {
-	int x, y, z, a;
-	while (1)
+	int N, M;
+	int max = 0;
+	int max1 = 0;
+	int val[102] = { 0 };
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++)
 	{
-		scanf("%d %d %d", &x, &y, &z);
-		if (x > y && y > z || x > z && z > y)
-		{
-			a = z;
-			z = x;
-			x = a;
-		}
-		else if (y > z && z > x || y > x && x > z)
-		{
-			a = z;
-			z = y;
-			y = a;
-		}
-		if (x == 0 && y == 0 && z == 0)
-		{
-			break;
-		}
-		if (x * x + y * y == z * z)
-		{
-			printf("right\n");
-		}
-		else 
-		{
-			printf("wrong\n");
-		}
-		
+		scanf("%d", &val[i]);
 	}
+	
+	for (int j = 0; j < N-2; j++)
+	{
+		for (int k = j+1; k < N; k++)
+		{
+			for (int q = k+1; q < N; q++)
+			{
+				max = val[j] + val[k] + val[q];
+				if (max <= M)
+				{
+					if (max1 < max)
+					{
+						max1 = max;
+					}
+				}
+				max = 0;
+			}
+		}
+	}	
+	printf("%d", max1);
 	return 0;
 }
