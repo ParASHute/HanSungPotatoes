ea7a2f3792272f4ac5bdd362da9fe22c24fa5be2
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-01-18 13:05:56 +0900

    1259 정답 (혼신의 if 똥꼬쇼)

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,22 +1,77 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-double fact(int a) {
-	if (a <= 1)
-	{
-		return 1;
-	}
-	else
-		return a * fact(a - 1);
-}
-
 int main() {
-	int T, N, M;
-	scanf("%d", &T);
-	for (int i = 0; i < T; i++)
+	int a, k = 0;
+	int b = 0;
+	while (1)
 	{
-		scanf("%d %d", &N, &M);
-		printf("%.0f\n", fact(M) / (fact(M - N) * fact(N)));
+		scanf("%d", &a);
+		if (a == 0)
+		{
+			break;
+		}
+		k = a;
+		if (k / 10 == 0)
+		{
+			printf("yes\n");
+		}
+		else if (k / 100 == 0)
+		{
+			if (k / 10 == k % 10)
+			{
+				printf("yes\n");
+			}
+			else
+			{
+				printf("no\n");
+			}
+		}
+		else if (k / 1000 == 0)
+		{
+			if (k / 100 == k % 10) {
+				printf("yes\n");
+			}
+			else
+			{
+				printf("no\n");
+			}
+		}
+		else if (k / 10000 == 0)
+		{
+			if (k / 1000 == k % 10) {
+				b = k / 100 - (k / 1000 * 10);
+				if (b == k % 100 / 10)
+				{
+					printf("yes\n");
+				}
+				else
+				{
+					printf("no\n");
+				}
+			}
+			else
+				printf("no\n");
+		}
+		else if (k / 100000 == 0)
+		{
+			if (k / 10000 == k % 10)
+			{
+				b = k / 1000 - (k / 10000 * 10);
+				if (b == k % 100 / 10)
+				{
+					printf("yes\n");
+				}
+				else
+				{
+					printf("no\n");
+				}
+			}
+			else
+			{
+				printf("no\n");
+			}
+		}
 	}
 	return 0;
 }
