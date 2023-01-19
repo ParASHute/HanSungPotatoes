cde8e8cf22acc90e401911d2f0c0c07f6b80cd73
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-20 00:07:40 +0900

    11931 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,25 +1,29 @@
-#include <stdio.h>  //개짱나는 문제.
+#include <stdio.h>
+#include <stdlib.h>  
 
-int main(void)
+//백준에서 동적할당으로 배열의 크기를 잡는 것과 그냥 처음부터 입력값의 최대치만큼 잡는것의
+//메모리 차이는 거의 없다. 이유는 무엇일까? 동적할당이 효율적인 것이 아닌건가?
+
+int static compare(const void* first, const void* second)
 {
-	int n, m, j, apple, basket, dis = 0;
-	scanf("%d %d", &n, &m);
-	basket = m;  
-	scanf("%d", &j);
-	while (j--)
-	{
-		scanf("%d", &apple);
-		if (apple > basket)  //사과 개수가 현재 바구니 위치보다 더 클때
-		{
-			dis += apple - basket;
-			basket = apple;
-		}
-		else if (apple < basket - (m - 1))
-		{
-			dis += basket - (m - 1) - apple;
-			basket = apple + (m - 1);
-		}
+    if (*(int*)first < *(int*)second)
+        return 1;
+    else if (*(int*)first > *(int*)second)
+        return -1;
+    else
+        return 0;
+}
+int main()
+{
+    int n;
+    int* a;
+    scanf("%d", &n);
+    a = (int*)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
 
-	}
-	printf("%d", dis);
+    qsort(a, n, 4, compare);
+    for (int i = 0; i < n; i++)
+        printf("%d\n", a[i]);
+    free(a);
 }
