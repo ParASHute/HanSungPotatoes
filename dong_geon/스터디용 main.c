839fd8003fc565a684f1a157efc2937bf1534e68
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-19 11:30:47 +0900

    15904 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,61 +1,33 @@
-#include <stdio.h>  //어려웠다 반례를 5개 맞아버림
+#include <stdio.h>
+#include <string.h>
+
+char str[1005];
 
 int main(void)
 {
-	int n, i, j, count[1005] = { 0 }, max = 0, student = 0;
-	int c1[1005], c2[1005], c3[1005], c4[1005], c5[1005];
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)  //i는 학생 번호
-		scanf("%d %d %d %d %d", &c1[i], &c2[i], &c3[i], &c4[i], &c5[i]);
-
-	for (i = 1; i <= n; i++)
+	int len, i, check[4] = { 0 };
+	fgets(str, 1005, stdin);
+	len = strlen(str);
+	for (i = 0; i < len; i++)
 	{
-		int check[1005] = { 0 };
-		for (j = 1; j <= n; j++)
-		{
-			if (i != j) {
-				if (c1[i] == c1[j]) {
-					if (check[j] == 0) {
-						count[i]++;
-						check[j] = 1;
-					}
-				}
-				if (c2[i] == c2[j]) {
-					if (check[j] == 0) {
-						count[i]++;
-						check[j] = 1;
-					}
-				}
-				if (c3[i] == c3[j]) {
-					if (check[j] == 0) {
-						count[i]++;
-						check[j] = 1;
-					}
-				}
-				if (c4[i] == c4[j]) {
-					if (check[j] == 0) {
-						count[i]++;
-						check[j] = 1;
-					}
-				}
-				if (c5[i] == c5[j]) {
-					if (check[j] == 0) {
-						count[i]++;
-						check[j] = 1;
-					}
-				}
-			}
+		if (str[i] == 'U')
+			check[0]++;
+		if (str[i] == 'C') {
+			if(check[0]>0)
+				check[1]++;
+		}
+		if (str[i] == 'P') {
+			if (check[1] > 0) 
+				check[2]++;
+		}
+		if (check[2] > 0) {
+			if (str[i] == 'C')
+				check[3]++;
 		}
 	}
-	for (i = 1; i <= n; i++) {
-		if (max < count[i]) {
-			max = count[i];
-			student = i;
-		}
-	}
-	if (max == 0)
-		printf("1");
+	if (check[0] > 0 && check[1] > 0 && check[2] > 0 && check[3] > 0)
+		printf("I love UCPC");
 	else
-		printf("%d", student);
-
+		printf("I hate UCPC");
+	
 }
