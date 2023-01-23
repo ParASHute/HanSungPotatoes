e3a1f0363da00269079499fdc248b549461e3936
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-23 12:13:13 +0900

    10822정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,8 +1,20 @@
-#include <stdio.h>  
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	int n;
-	scanf("%d", &n);
-	printf("%d", n * (n - 1) * (n - 2) * (n - 3) / 24);
+	int i, j, len, sum = 0, plus = 1;
+	char s[101];
+	scanf("%s", s);
+	len = strlen(s);
+	for (i = len-1; i >= 0; i--)
+	{
+		if (s[i] == ',') {
+			plus = 1;
+			continue;
+		}
+		sum += (plus * (s[i]-'0'));
+		plus *= 10;
+	}
+	printf("%d", sum);
 }
