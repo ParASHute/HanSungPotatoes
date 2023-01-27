88ec572ac0f8907d0647e5e9fad613b6342da882
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-27 12:23:45 +0900

    1094 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int N[1000001] = { 0 };  //이게 반복문 while 안에 있어야 초기화 되는거 아닌가?
-int M[1000001] = { 0 };  //초기화 안하고도 통과가 되네? 
-
-int static compare(const void* first, const void* second)
-{
-	if (*(int*)first > *(int*)second)
-		return 1;
-	else if (*(int*)first < *(int*)second)
-		return -1;
-	else
-		return 0;
-}
 
 int main(void)
 {
-	int t, n, m, i;
-	scanf("%d", &t);
-	while (t--)
+	int x, s = 64, stick = 0, cnt = 0;
+	scanf("%d", &x);
+	while (1)
 	{
-		scanf("%d %d", &n, &m);
-		for (i = 0; i < n; i++)
-			scanf("%d", &N[i]);
-		for (i = 0; i < m; i++)
-			scanf("%d", &M[i]);
-		qsort(N, n, 4, compare);
-		qsort(M, m, 4, compare);
-		if (N[n - 1] >= M[m - 1])
-			printf("S\n");
-		else
-			printf("B\n");
+		if (s <= x-stick) {
+			stick += s;
+			cnt++;
+		}	
+
+		if (stick == x) {
+			printf("%d", cnt);
+			break;
+		}
+
+		s /= 2;
 	}
 }
