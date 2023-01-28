4e9f272d7c766fb6e9b69037035e92ee346af9b4
DESKTOP-TAFEUKF\LDK <dkms625@naver.com>
2023-01-28 13:09:10 +0900

    백준 11655 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	int x, i, j; // i와 j는 반복문
-	int cnt = 0, y = 1;  //cnt는 순서 체크, y는 대각선 줄을 의미:초기값 1
-	scanf("%d", &x);
-
-	while(1)   //무한반복
+	int i, len;
+	char s[101], rot[101];
+	fgets(s, 101, stdin);
+	len = strlen(s);
+	for (i = 0; i < len; i++)
 	{
-		for (i = 1; i <= y; i++) //이 식이 만들어진 이유: 첫번째 대각선에 개수는 1개, 두번째 대각선 2개, 세번째 3개 ...로 이루어짐
-		{
-			cnt++;
-			if (cnt == x) //cnt가 늘다가 x를 만나면 for문에서 break
-				break;
-		}
-		
-		if (cnt == x) //위에서 break 되었다면 while문 탈출위한 break
-			break;
-
-		y++;  //다음 대각선으로 넘어가는 과정
+		if (s[i] >= 65 && s[i] <= 77)
+			rot[i] = s[i] + 13;
+		else if (s[i] >= 78 && s[i] <= 90)
+			rot[i] = s[i] -13;
+		else if (s[i] >= 97 && s[i] <= 109)
+			rot[i] = s[i] + 13;
+		else if (s[i] >= 110 && s[i] <= 122)
+			rot[i] = s[i] -13;
+		else
+			rot[i] = s[i];
 	}
-
-
-	if (y % 2 == 0)  // x번째 대각선 x가 짝수면 
-		printf("%d/%d", i, y - i + 1);  //탈출 된 시점 기준으로 i, y-i+1이 된다.
-
-	else   //x번째 대각선 x가 홀수면 
-		printf("%d/%d", y - i + 1, i);
-
+	rot[len] = '\0';
+	printf("%s", rot);
 }
