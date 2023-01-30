ec9fec9ee5e0389600ee7484ed479fa81f7b57c3
MoongTeng <dkms625@naver.com>
2023-01-30 21:44:07 +0900

    동건

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -3,18 +3,6 @@
 
 int main(void)
 {
-	int i, len;
-	char s[1001], change[1001];
-	scanf("%s", s);
-	len = strlen(s);
-	for (i = 0; i < len; i++)
-	{
-		if (s[i] >= 68 && s[i] <= 90)
-			change[i] = (s[i] - 3);
-		else
-			change[i] = (s[i] + 23);
-	}
-	change[len] = '\0';
-	printf("%s", change);
-	return 0;
+	printf(" git hub 연동 성공했나? 하..\n");
+	printf("동생 노트북 훔침\n");
 }
