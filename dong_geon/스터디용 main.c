c0adb3e15ae812b7eb164376a7f34417fa95f520
MoongTeng <dkms625@naver.com>
2023-01-31 20:07:40 +0900

    5555 많이 어려웠던 문제..

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-	int n, i[101], cnt = 0;
+int main(void)
+{
+	char find[11], ring[100][11];
+	int i, n, temp, len, cnt = 0;
+	scanf("%s", find);
 	scanf("%d", &n);
-	for (int j = 0; j < n; j++)	{
-		scanf("%d", &i[j]);
-		if (i[j] == 1)
-			cnt++;
+	len = strlen(find);
+	for (i = 0; i < n; i++) {
+
+		scanf("%s", ring[i]);
+
+		for (int j = 0; j < 10; j++)
+		{
+			int k = 0, z = 0;
+			temp = 0;
+			if (find[k] == ring[i][j])  //첫 문자가 같다면
+			{
+				z = j;
+				while (find[k] == ring[i][z])  //문자가 다르면 탈출
+				{
+					z++;
+					k++;
+					temp++;
+					if (z == 10)  //ring가 10이 넘어가면 0부터
+						z = 0;
+				}
+
+				if (temp == len)
+				{
+					cnt++;
+					break;
+				}
+			}
+		}
 	}
-	if (n / 2 < cnt)
-		printf("Junhee is cute!");
-	else
-		printf("Junhee is not cute!");
+
+	printf("%d", cnt);
+
+	return 0;
 }
