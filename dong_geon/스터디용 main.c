ed1c36bd8f49fad2a608c8dba64ab03e33d8d22f
MoongTeng <dkms625@naver.com>
2023-02-03 01:33:07 +0900

    2436 공약수(할만한 골드5)

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 
+long long gcd(long long x, long long y)  //return 최대공약수
+{
+	if (y == 0)
+		return x;
+	else
+		return gcd(y, x % y);
+}
+
 int main(void)
 {
-	int n, i, h[50], w[50];
-	scanf("%d", &n);
-	for (i = 0; i < n; i++)
-		scanf("%d %d", &h[i], &w[i]);
-	for (i = 0; i < n; i++)
-	{
-		int score = 1;
-		for (int j = 0; j < n; j++)
+	long long i, a, b, mul, x, sum = 10000000000000000;
+	long long A = 0, B = 0;
+	scanf("%lld %lld", &a, &b);
+	mul = a * b;
+	if (a == b)
+		printf("%lld %lld", a, b);
+	else {
+		for (i = a; i<=b; i++)
 		{
-			if (h[i] < h[j] && w[i] < w[j])
-				score++;
+			x = mul / i;
+			if (x * i == mul && gcd(x, i) == a) {
+				if (sum > i + x) {
+					sum = i + x;
+					A = i;
+					B = x;
+				}
+			}
 		}
-		printf("%d ", score);
+		printf("%lld %lld", A, B);
 	}
 }
