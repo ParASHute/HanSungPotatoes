434435d9ace1e5315298622f9dd1dd8818e84857
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-02-02 19:46:54 +0900

    1380 귀걸이 시간초과 도움

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,54 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int a[1002] = { 0 };
-int b[1002] = { 0 };
+char str[100][60];
+char num[200][60];
+
 
 int main() {
-	int N, K, A, j = 0, B;
-	int count = 0;
-	scanf("%d %d", &N, &K);
-	A = K;
-	B = N;
-	for (int i = 0; i < N; i++)
-	{
-		a[i] = i + 1;
-	}
-	printf("<");
-	while (j < N)
-	{
-		for (int i = 0; i < N; i++) {
-			if (K == a[i]) {
-				b[j] = a[i];
-				a[i] = 0;
-				K += A;
-				printf("%d, ", b[j]);
-				j++;
-			}
-			if (K > N)
+	int n, count = 0;
+	char arr;
+	while (1) {
+		int num1[200] = { 0 };
+		scanf("%d", &n);
+		while (getchar() != '\n');
+		count++;
+		if (n == 0)
+		{
+			break;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			fgets(str[i], 60, stdin);
+		}
+		for (int i = 0; i < n; i++)
+		{
+			scanf(" %[^\n]s", num[i]);
+		}
+		if (n == 1)
+		{
+			for (int i = 0; i < 1; i++)
 			{
-				if (a[K-1] == a[i])
-				{
-					if (a[i] != 0) {
-						b[j] = a[i];
-						a[i] = 0;
-						K += A;
-						printf("%d", b[j]);
-						if (j < N-1)
-						{
-							printf(", ");
-						}
-						j++;
-					}
-				}
+				scanf(" %[^\n]s", num[i + n]);
+				num1[num[i + n][0] - 49] = 1;
 			}
-			if (a[i] != 0)
+		}
+		else {
+			for (int i = 0; i < n - 1; i++)
 			{
-				a[N + count] = a[i];
-				count++;
+				scanf(" %[^\n]s", num[i + n]);
+				num1[num[i + n][0] - 49] = 1;
+			}
+		}
+		for (int i = 0; i < n; i++)
+		{
+			if (num1[i] == 0)
+			{
+				printf("%d %s", count, str[i]);
 			}
 		}
 	}
-	printf(">");
 	return 0;
 }
