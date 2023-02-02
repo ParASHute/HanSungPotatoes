666a9abe9ed549b074ce8c09ab67e9adc6b0dcf1
MoongTeng <dkms625@naver.com>
2023-02-03 00:43:25 +0900

    7568 덩치 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-#include <string.h>
-int main() 
-{
-	int t, i, beauty;
-	char n[11];
-	scanf("%d", &t);
-	while (t--)
-	{
-		int num[10] = { 0 };
-		beauty = 0;
-		scanf("%s", n);
-		for (i = 0; i < strlen(n); i++)
-			num[n[i] - 48]++;
 
-		for (i = 0; i < 10; i++)
+int main(void)
+{
+	int n, i, h[50], w[50];
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+		scanf("%d %d", &h[i], &w[i]);
+	for (i = 0; i < n; i++)
+	{
+		int score = 1;
+		for (int j = 0; j < n; j++)
 		{
-			if (num[i] > 0)
-				beauty++;
+			if (h[i] < h[j] && w[i] < w[j])
+				score++;
 		}
-		printf("%d\n", beauty);
+		printf("%d ", score);
 	}
 }
