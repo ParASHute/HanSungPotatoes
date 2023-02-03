c222d35bfaf67231cfa102f217d57f6534fd620f
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-02-03 11:33:46 +0900

    1380 정답 (귀걸이 저주한다)

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,17 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-char str[100][60];
-char num[200][60];
-
 
 int main() {
 	int n, count = 0;
 	char arr;
 	while (1) {
+		char str[100][60];
+		int a;
 		int num1[200] = { 0 };
 		scanf("%d", &n);
-		while (getchar() != '\n');
+		getchar();
 		count++;
 		if (n == 0)
 		{
@@ -19,32 +18,18 @@ int main() {
 		}
 		for (int i = 0; i < n; i++)
 		{
-			fgets(str[i], 60, stdin);
+			gets(str[i]);
+		}
+		for (int i = 0; i < 2*n-1; i++)
+		{
+			scanf("%d %c", &a, &arr);
+			num1[a - 1]++;
 		}
 		for (int i = 0; i < n; i++)
 		{
-			scanf(" %[^\n]s", num[i]);
-		}
-		if (n == 1)
-		{
-			for (int i = 0; i < 1; i++)
+			if (num1[i] == 1)
 			{
-				scanf(" %[^\n]s", num[i + n]);
-				num1[num[i + n][0] - 49] = 1;
-			}
-		}
-		else {
-			for (int i = 0; i < n - 1; i++)
-			{
-				scanf(" %[^\n]s", num[i + n]);
-				num1[num[i + n][0] - 49] = 1;
-			}
-		}
-		for (int i = 0; i < n; i++)
-		{
-			if (num1[i] == 0)
-			{
-				printf("%d %s", count, str[i]);
+				printf("%d %s\n", count, str[i]);
 			}
 		}
 	}
