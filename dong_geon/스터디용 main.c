7da3eebe814a55f31192ad6f632b8bfe86590dce
MoongTeng <dkms625@naver.com>
2023-02-05 15:18:22 +0900

    11728 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
-//생각보다 좀 까다로운 문제
+#include <stdlib.h>
 
+int static compare(const void* first, const void* second)
+{
+	if (*(int*)first > *(int*)second)
+		return 1;
+	else if (*(int*)first < *(int*)second)
+		return -1;
+	else
+		return 0;
+}
 int main() {
-	int m[10], n, sum[10] = { 0 }, dif[10] = { 0 };
-
-	for (int i = 0; i < 10; i++)
-		scanf("%d", &m[i]);
-
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j <= i; j++) {
-			sum[i] += m[j];
-			if (sum[i] >= 100)
-				break;
-		}
-
-		if (sum[i] < 100)
-			dif[i] = 100 - sum[i] + 1;
-		else
-			dif[i] = sum[i] - 100;
-	}
-	int min_dif = 100;
-	int index;
-	for (int i = 0; i < 10; i++)
-	{
-		if (dif[i] < min_dif) {
-			min_dif = dif[i];
-			index = i;
-		}
-	}
-	printf("%d", sum[index]);
+	int n, m, i;
+	int* a;
+	scanf("%d %d", &n, &m);
+	a = (int*)malloc(sizeof(int) * (n + m));
+	for (i = 0; i < n + m; i++)
+		scanf("%d", &a[i]);
+	qsort(a, n + m, 4, compare);
+	for (i = 0; i < n + m; i++)
+		printf("%d ", a[i]);
 }
