d14f3dbb4d01284fe7b8bf6fcf5882ef3bec717a
MoongTeng <dkms625@naver.com>
2023-02-06 02:01:43 +0900

    18511 어려웠던 문제

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-//실행시간을 측정해주는 코드 (time.h 를 꼭 추가해주어야 함)
 
-int main() {
-	int x = 0;
-	clock_t start, stop;
-	double duration;
-
-	start = clock();   //여기서부터 시작
-	//시간을 계산하고자 하는 코드
-	for (int i = 0; i < 10000; i++)
-	{
-		for (int j = 0; j < 10000; j++)
-			x++;
+int main()
+{
+	int k, n, x, tmp;
+	int num[10] = { 0 };
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < k; i++) {
+		scanf("%d", &x);
+		num[x] = 1;
 	}
-	stop = clock();  //여기까지 시간측정
-
-	duration = (double)(stop - start) / CLOCKS_PER_SEC;
-	printf("%d %lf초가 걸림", x, duration);
+	
+	for (int i = n; i > 0; i--)
+	{
+		tmp = 1;
+		for (int j = i; j > 0; j /= 10)
+		{
+			if (num[j % 10] != 1)
+			{
+				tmp = 0;
+				break;
+			}
+		}
+		if (tmp == 1) {
+			printf("%d", i);
+			break;
+		}
+	}
 }
