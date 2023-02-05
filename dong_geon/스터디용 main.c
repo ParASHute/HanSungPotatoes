06d4a7279d5f12d49a6e6926d0441146ffec25bd
MoongTeng <dkms625@naver.com>
2023-02-05 15:37:38 +0900

    자신의 코드 시간 계산하기 (시간 초과시 사용해보세요.)

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+//실행시간을 측정해주는 코드 (time.h 를 꼭 추가해주어야 함)
 
-int static compare(const void* first, const void* second)
-{
-	if (*(int*)first > *(int*)second)
-		return 1;
-	else if (*(int*)first < *(int*)second)
-		return -1;
-	else
-		return 0;
-}
 int main() {
-	int n, m, i;
-	int* a;
-	scanf("%d %d", &n, &m);
-	a = (int*)malloc(sizeof(int) * (n + m));
-	for (i = 0; i < n + m; i++)
-		scanf("%d", &a[i]);
-	qsort(a, n + m, 4, compare);
-	for (i = 0; i < n + m; i++)
-		printf("%d ", a[i]);
+	int x = 0;
+	clock_t start, stop;
+	double duration;
+
+	start = clock();   //여기서부터 시작
+	//시간을 계산하고자 하는 코드
+	for (int i = 0; i < 10000; i++)
+	{
+		for (int j = 0; j < 10000; j++)
+			x++;
+	}
+	stop = clock();  //여기까지 시간측정
+
+	duration = (double)(stop - start) / CLOCKS_PER_SEC;
+	printf("%d %lf초가 걸림", x, duration);
 }
