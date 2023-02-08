ec6de5fb25e78986da61480e70b13bd909703cfe
ParASHute <ktwykhs@naver.com>
2023-02-08 12:59:09 +0900

    10825번 정답

--- algorithm/algorithm/main.c
+++ algorithm/algorithm/main.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
-int main(){
-    int arr[100000] = {0};
-    int n,a;
-    int k = 0, count = 0, sum = 0;
-    scanf("%d",&n);
-    while(1){
-        scanf("%d",&a);
-        count++;
-        if(a == 0){
-            k--;
-            arr[k] = 0;
+#include <stdlib.h>
+#include <string.h>
+//#include <time.h>
+typedef struct{
+    char str[10];
+    int kor;
+    int eng;
+    int mat;
+} grade;
+int compare(const void* fst, const void* snd){
+    grade F = *(grade*)fst;
+    grade S = *(grade*)snd;
+    if(F.kor < S.kor) return 1;
+    else if(F.kor == S.kor){
+        if(F.eng > S.eng) return 1;
+        else if(F.eng ==  S.eng){
+            if(F.mat < S.mat) return 1;
+            else if(F.mat == S.mat) return strcmp(F.str, S.str);
+            else return -1;
         }
-        else{
-            arr[k] = a;
-            k++;
-        }
-        if(count == n) break;
+        else return -1;
     }
-    for(int i = 0; i < n; i++) sum += arr[i];
-    printf("%d",sum);
+    else return -1;
 }
+int main(){
+    grade arr[100000] = {'\0'};
+    int n;
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++){
+        scanf("%s %d %d %d", arr[i].str, &arr[i].kor, &arr[i].eng, &arr[i].mat);
+    }
+    qsort(arr, n, sizeof(grade), compare);
+    for(int i = 0; i < n; i++) printf("%s\n",arr[i].str);
+}
+//    clock_t start, finish;
+//    double duration;
+//    start = clock();
+//    함수 위치
+//    finish = clock();
+//    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+//    printf("%fsec\n",duration);
