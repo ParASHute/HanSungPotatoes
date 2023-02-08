e34ef96cde9d65133d33f513c22feb4bb53c8ebc
Flumeys <118504727+Flumeys@users.noreply.github.com>
2023-02-08 14:30:37 +0900

    1009번 문제

--- sang_hyun/main.c
+++ sang_hyun/main.c
@@ -152,11 +152,22 @@ int main() {
 //1009번 (2/6일)
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<math.h>
+
 int main() {
-	int a,x,y,n;
+	int a, n;
+	double x, y;
 	scanf("%d", &a);
+
 	for (int i = 0; i < a; i++) {
-		scanf("%d %d", &x, &y);
-		n = (x ^ y) % 10;
+		scanf("%lf %lf", &x, &y);
+		n = (int)pow((double)x, (double)y)%10;
+
+		if (n == 0) {
+			printf("10\n");
+		}
+		else {
+			printf("%d\n", n);
+		}
 	}
 }
