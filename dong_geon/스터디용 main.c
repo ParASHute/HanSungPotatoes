0af47a29ec5bf3fb721108a088436c534e75b636
MoongTeng <dkms625@naver.com>
2023-02-11 01:31:05 +0900

    3036 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,26 +1,24 @@
 #include <stdio.h> 
-#include <string.h>
+#include <stdlib.h>
 
-char s[100001];
-char t[100001];
+int gcd(int a, int b)
+{
+	int i, gcd = 1;
+	for (i = 1; i <= a && i <= b; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+			gcd = i;
+	}
+	return gcd;
+}
+
 int main(void)
 {
-	int slen, tlen, i;
-	while (scanf("%s %s", s, t)!=EOF) {
-		slen = strlen(s);
-		tlen = strlen(t);
-		
-		int count = 0;
-		for (i = 0; i < tlen; i++)
-		{
-			if (s[count] == t[i])
-				count++;
-			if (count == slen) {
-				printf("Yes\n");
-				break;
-			}
-		}
-		if (count != slen)
-			printf("No\n");
-	}
+	int n, ring[101];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &ring[i]);
+	for (int i = 1; i < n; i++)
+		printf("%d/%d\n", ring[0] / gcd(ring[0], ring[i]), ring[i] / gcd(ring[0], ring[i]));
+	return 0;
 }
