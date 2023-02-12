b2cdd0fe3000e4713749446ce2d5deb66242fa51
MoongTeng <dkms625@naver.com>
2023-02-12 17:34:15 +0900

    1018 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,24 +1,37 @@
-#include <stdio.h> 
-#include <stdlib.h>
-
-int gcd(int a, int b)
-{
-	int i, gcd = 1;
-	for (i = 1; i <= a && i <= b; i++)
-	{
-		if (a % i == 0 && b % i == 0)
-			gcd = i;
-	}
-	return gcd;
-}
+#include <stdio.h>
 
 int main(void)
 {
-	int n, ring[101];
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &ring[i]);
-	for (int i = 1; i < n; i++)
-		printf("%d/%d\n", ring[0] / gcd(ring[0], ring[i]), ring[i] / gcd(ring[0], ring[i]));
-	return 0;
+	char chess[51][51];
+	int x, y, i, j, min = 65;
+	scanf("%d %d", &x, &y);
+	for (i = 0; i < x; i++) {
+		scanf("%s", chess[i]);
+	}
+
+	for (i = 0; i < x-7; i++) {
+		for (j = 0; j < y-7; j++) {
+			int white = 0;
+			int black = 0;
+			for (int z = i; z < i + 8; z++) {
+				for (int k = j; k < j + 8; k++) {
+					if ((z + k) % 2 == 0) {
+						if (chess[z][k] == 'B')
+							white++;
+						else
+							black++;
+					}
+					else {
+						if (chess[z][k] == 'B')
+							black++;
+						else
+							white++;
+					}
+				}
+			}
+			min = black > min ? min : black;
+			min = white > min ? min : white;
+		}
+	}
+	printf("%d", min);
 }
