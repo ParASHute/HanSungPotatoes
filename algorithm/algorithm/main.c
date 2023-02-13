d0412e9144325a8e0d6ffaf7b68cde501debd933
ParASHute <ktwykhs@naver.com>
2023-02-14 07:43:31 +0900

    11656 접미사 배열 정답

--- algorithm/algorithm/main.c
+++ algorithm/algorithm/main.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
+#include <stdlib.h>
+#include <string.h>
 //#include <time.h>
 //typedef struct{
-//    int z;
-//    int y;
-//}name;
-int gcd(int a, int b){
-    if (a % b == 0) return b;
-    else return gcd(b, a % b);
+//    char name[21];
+//    char pw[21];
+//}P;
+int compare(const void* fst, const void* snd){
+    return strcmp((char*)fst, (char*)snd);
 }
 int main(){
-    int n,a;
-    int arr[100] = {0};
-    scanf("%d",&n);
-    for(int i = 0; i < n; i++){
-        long long sum = 0;
-        scanf("%d",&a);
-        for(int j = 0; j < a; j++) scanf("%d",&arr[j]);
-        for(int k = 0; k < a - 1; k++){
-            for(int l = k + 1; l < a; l++){
-                sum += gcd(arr[k], arr[l]);
-            }
+    char str[10001];
+    char suf[1001][1001];
+    gets(str);
+    int count = strlen(str);
+    for (int i = 0; i < count; i++) {
+            strcpy(suf[i], str);
+            for (int j = 0; j < strlen(str); j++) str[j] = str[j + 1];
         }
-        printf("%lld\n",sum);
-    }
+    qsort(suf, count, sizeof(suf[0]), compare);
+    for (int i = 0; i < count; i++) printf("%s\n", suf[i]);
 }
 //    clock_t start, finish;
 //    double duration;
