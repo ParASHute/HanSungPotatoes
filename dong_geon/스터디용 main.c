e5157c570ab6f9090f1e727d8565858912807f15
MoongTeng <dkms625@naver.com>
2023-02-14 15:29:42 +0900

    2160 3차원배열 처음 써본 문제

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(void)
+int main()
 {
-	char arr[6][16] = { 0 };
-	int len = 0;
-	for (int i = 0; i < 5; i++) {
-		scanf("%s", arr[i]);
-		len += strlen(arr[i]);
+	char pic[50][5][7];
+	char ans[5][7];
+	int n, i, j;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < 5; j++)
+			scanf("%s", pic[i][j]);
 	}
-
-	int index = 0, plus = 0;
+	int index = 0;
+	int a=0, b=0, cnt, max = -1;
 	while (1) {
-		for (int i = 0; i < 5; i++)
-		{
-			if (plus == len)
-				break;
-			if (arr[i][index] != '\0') {
-				printf("%c", arr[i][index]);
-				plus++;
+		if (index == n)
+			break;
+
+		for (i = 0; i < 5; i++) {
+			for (j = 0; j < 7; j++) {
+				ans[i][j] = pic[index][i][j];
 			}
 		}
-		if (plus == len)
-			break;
+		
+		for (int k = 0; k < n; k++) {
+			cnt = 0;
+			if (k == index)
+				continue;
+			for (i = 0; i < 5; i++) {
+				for (j = 0; j < 7; j++) {
+					if (ans[i][j] == pic[k][i][j])
+						cnt++;
+				}
+			}
+			if (cnt > max)
+			{
+				max = cnt;
+				a = index + 1;
+				b = k + 1;
+			}
+		}
+
 		index++;
 	}
+	if (a < b)
+		printf("%d %d", a, b);
+	else
+		printf("%d %d", b, a);
 }
