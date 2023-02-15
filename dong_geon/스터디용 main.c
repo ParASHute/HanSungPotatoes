fcb53a714ef38c0c938689bffcbfe8bf93a6cdfb
MoongTeng <dkms625@naver.com>
2023-02-15 12:51:58 +0900

    브1 2729 왜틀렸는지 찾아주실분..

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,49 +1,87 @@
 #include <stdio.h>
+#include <string.h>
+
+char a[81]={0};
+char b[81]={0};
+char result[85]={0};
+
+void reverse(char a[])
+{
+	int i, len;
+	len = strlen(a);
+	for (i = 0; i < len / 2; i++)
+	{
+		char temp = a[i];
+		a[i] = a[len - 1 - i];
+		a[len - i - 1] = temp;
+	}
+}
+
+void sum(char* a, char* b) {
+	int i, longlen, shortlen, plus = 0, sum = 0;
+	longlen = strlen(a) >= strlen(b) ? strlen(a) : strlen(b);
+	shortlen = strlen(a) <= strlen(b) ? strlen(a) : strlen(b);
+	reverse(a);
+	reverse(b);
+
+	for (i = 0; i < longlen; i++)
+	{
+		if (i < shortlen) {  //둘다 값이 있을 떄
+			sum = (a[i] - '0') + (b[i] - '0') + plus;
+			if (sum > 1)
+				plus = 1; //plus는 다음 값 1추가 해야할때 쓰는 변수
+			else
+				plus = 0;
+		}
+		else  //둘 중 하나가 없음
+		{
+			if (a[i] - '0' >= 0) {  //a가 더 길 때
+				sum = a[i] - '0' + plus;
+				if (sum > 1)
+					plus = 1;
+				else
+					plus = 0;
+			}
+			else if (b[i] - '0' >= 0) {  //b가 더 길 때
+				sum = b[i] - '0' + plus;
+				if (sum > 1)
+					plus = 1;
+				else
+					plus = 0;
+			}
+		}
+		result[i] = sum % 2 + '0';
+	}
+
+	if (plus == 1)
+		result[longlen] = '1';
+
+	reverse(result);
+	//쓸모없는 0을 지우는 코드
+	int check = 0;
+	for (i = 0; i < strlen(result); i++) {
+		if (result[i] == '1') {
+			check = 1;
+			printf("%c", result[i]);
+		}
+		if (result[i] == '0' && check == 1)
+			printf("%c", result[i]);
+	}
+
+	if (check == 0)
+		printf("0\n");
+	else
+		printf("\n");
+}
 
 int main()
 {
-	char pic[50][5][7];
-	char ans[5][7];
-	int n, i, j;
+	int n, i;
 	scanf("%d", &n);
 	for (i = 0; i < n; i++) {
-		for (j = 0; j < 5; j++)
-			scanf("%s", pic[i][j]);
+		scanf("%s %s", a, b);
+		for (int i = 0; i < 85; i++)
+			result[i] = '\0';
+		sum(a, b);
 	}
-	int index = 0;
-	int a=0, b=0, cnt, max = -1;
-	while (1) {
-		if (index == n)
-			break;
-
-		for (i = 0; i < 5; i++) {
-			for (j = 0; j < 7; j++) {
-				ans[i][j] = pic[index][i][j];
-			}
-		}
-		
-		for (int k = 0; k < n; k++) {
-			cnt = 0;
-			if (k == index)
-				continue;
-			for (i = 0; i < 5; i++) {
-				for (j = 0; j < 7; j++) {
-					if (ans[i][j] == pic[k][i][j])
-						cnt++;
-				}
-			}
-			if (cnt > max)
-			{
-				max = cnt;
-				a = index + 1;
-				b = k + 1;
-			}
-		}
-
-		index++;
-	}
-	if (a < b)
-		printf("%d %d", a, b);
-	else
-		printf("%d %d", b, a);
 }
