5237cdb74a22ff44e64823e8a90c66da132111f5
MoongTeng <dkms625@naver.com>
2023-02-17 01:16:37 +0900

    9461 정답

--- dong_geon/스터디용 main.c
+++ dong_geon/스터디용 main.c
@@ -1,62 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 
-int stack[10001] = { 0 };
-int idx = 0;
-
-void push(int n) {
-	stack[idx] = n;
-	idx++;
+long long DP[101] = { 0 };
+void dp(void) {
+	DP[1] = 1;
+	DP[2] = 1;
+	DP[3] = 1;
+	DP[4] = 2;
+	for (int i = 5; i <= 100; i++)
+		DP[i] = DP[i - 2] + DP[i - 3];
 }
-void pop(void) {
-	if (idx == 0)
-		printf("-1\n");
-	else if (idx > 0) {
-		idx--;
-		printf("%d\n", stack[idx]);
-		stack[idx] = 0;
-	}
-}
-void size(void) {
-	printf("%d\n", idx);
-}
-void empty(void) {
-	if (idx == 0)
-		printf("1\n");
-	else
-		printf("0\n");
-}
-void top(void) {
-	if (idx == 0)
-		printf("-1\n");
-	else
-		printf("%d\n", stack[idx - 1]);
-}
-
 
 int main()
 {
-	int n, i, num;
-	char s[10];
-	scanf("%d", &n);
-	for (i = 0; i < n; i++)
-	{
-		scanf("%s", s);
-		if (strcmp(s, "push") == 0) {
-			scanf("%d", &num);
-			push(num);
-		}
-		if (strcmp(s, "pop") == 0) {
-			pop();
-		}
-		if (strcmp(s, "size") == 0) {
-			size();
-		}
-		if (strcmp(s, "empty") == 0) {
-			empty();
-		}
-		if (strcmp(s, "top") == 0) {
-			top();
-		}
+	int t, n, i;
+	scanf("%d", &t);
+	dp();
+	while (t--) {
+		scanf("%d", &n);
+		printf("%lld\n", DP[n]);
 	}
 }
