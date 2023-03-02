93ab22413a0335ef842f8508e399656e335d75ae
ParASHute <ktwykhs@naver.com>
2023-03-02 19:35:48 +0900

    (2020년 삼성입사 문제) 14501

--- algorithm/algorithm/main.c
+++ algorithm/algorithm/main.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//#include <stdlib.h>
+//#include <string.h>
 //#include <time.h>
 //typedef struct{
-//    char name[21];
-//    char pw[21];
-//}P;
-int compare(const void* fst, const void* snd){
-    return strcmp((char*)fst, (char*)snd);
-}
+//    int s;
+//    int r;
+//}list;
 int main(){
-    char str[10001];
-    char suf[1001][1001];
-    gets(str);
-    int count = strlen(str);
-    for (int i = 0; i < count; i++) {
-            strcpy(suf[i], str);
-            for (int j = 0; j < strlen(str); j++) str[j] = str[j + 1];
+    int n;
+    int max = 0;
+    int P[16] = {0};
+    int T[16] = {0};
+    scanf("%d",&n);
+    for(int i = 1; i <= n; i++){
+        scanf("%d %d", &T[i], &P[i]);
+    }
+    for(int i = 1; i <= n; i++){
+        int k = i;
+        int count = 0;
+        while(1){
+            if(k > n) break;
+            if(k == n && T[k] != 1 && k+T[k] > n) break;
+            count += P[k];
+            k += T[k];
         }
-    qsort(suf, count, sizeof(suf[0]), compare);
-    for (int i = 0; i < count; i++) printf("%s\n", suf[i]);
+        max = count > max ? count : max;
+    }
+    printf("%d",max);
 }
+
+// 4번 케이스에 있어서  5 4 3 2 1 1(2) 2(2) 3(2) 4(2) 5(2)로 들어가는데
+// 5 1(2) 2가 아닌 5 1(2) 3(2)이 되야 한다 -> 이경우를 잡아야 함
+
 //    clock_t start, finish;
 //    double duration;
 //    start = clock();
