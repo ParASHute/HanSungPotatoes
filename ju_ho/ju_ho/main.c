48dddb7719a2f810a17b983ed44c15164e1dcb69
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-03-13 11:31:49 +0900

    9012 정답

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,40 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-	int x;
-	int y;
-}xy;
-
-int static compare(const void* fir, const void* sec) {
-	xy F = *(xy*)fir;
-	xy S = *(xy*)sec;
-	if (F.x > S.x)
-		return 1;
-	else if (F.x < S.x)
-		return -1;
-	else {
-		if (F.y > S.y)
-			return 1;
-		else if (F.y < S.y)
-			return -1;
-		else
-			return 0;
-	}
-}
-
+#include <string.h>
 int main() {
-	int N;
-	xy arr[100000] = { 0 };
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++)
+	int T,count1 = 0,count2 = 0;
+	char vps[50] = { '\0' };
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++)
 	{
-		scanf("%d %d", &arr[i].x,&arr[i].y);
+		scanf("%s", vps);
+		getchar();
+		for (int j = 0; j < strlen(vps); j++)
+		{
+			if (vps[j] == '(')
+			{
+				count1++;
+			}
+			else
+				count2++;
+			if (count1 < count2)
+			{
+				printf("NO\n");
+				break;
+			}
+		}
+		if (count1 == count2)
+		{
+			printf("YES\n");
+		}
+		else if (count1 > count2) printf("NO\n");
+		count1 = 0;
+		count2 = 0;
 	}
-	qsort(arr, N, sizeof(xy), compare);
-	for (int i = 0; i < N; i++)
-	{
-		printf("%d %d\n", arr[i].x,arr[i].y);
-	}
+	return 0;
 }
