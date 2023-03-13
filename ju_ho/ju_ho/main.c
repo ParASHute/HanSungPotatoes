73dbc1e51eff38127c5e541340032c7842f2e77f
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-03-13 11:10:29 +0900

    11650 (구조체 정렬,qsort)

--- ju_ho/ju_ho/main.c
+++ ju_ho/ju_ho/main.c
@@ -1,29 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
+#include <stdlib.h>
 
+typedef struct {
+	int x;
+	int y;
+}xy;
+
+int static compare(const void* fir, const void* sec) {
+	xy F = *(xy*)fir;
+	xy S = *(xy*)sec;
+	if (F.x > S.x)
+		return 1;
+	else if (F.x < S.x)
+		return -1;
+	else {
+		if (F.y > S.y)
+			return 1;
+		else if (F.y < S.y)
+			return -1;
+		else
+			return 0;
+	}
+}
 
 int main() {
-	int N, K, a = 1, k = 0;
-	int arr[1001] = { 0 };
-	scanf("%d %d", &N, &K);
-	for (int i = 2; i <= N; i++)
+	int N;
+	xy arr[100000] = { 0 };
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = i; j <= N; j+=i)
-		{
-			arr[k] = j;
-			for (int q = 0; q < k; q++)
-			{
-				if (arr[q] == j) {
-					arr[q] == 0;
-					k--;
-					break;
-				}
-			}
-			k++;
-		}
+		scanf("%d %d", &arr[i].x,&arr[i].y);
 	}
-	printf("%d", arr[K-1]);
-	return 0;
+	qsort(arr, N, sizeof(xy), compare);
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d %d\n", arr[i].x,arr[i].y);
+	}
 }
