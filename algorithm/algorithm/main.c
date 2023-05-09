01472318d1c062c91cb39bdb6ec2cce2a8138b62
ParASHute <ktwykhs@naver.com>
2023-05-09 15:15:45 +0900

    test

--- algorithm/algorithm/main.c
+++ algorithm/algorithm/main.c
@@ -6,31 +6,22 @@
 //    int s;
 //    int r;
 //}list;
+int arr[1000001];
+int fibo(int n){
+    if (n < 2)
+        return n;
+    else if (n == 2)
+        return 2;
+    else if (arr[n] == 0)
+        arr[n] = (fibo(n - 1) + fibo(n - 2)) % 15746;
+    return arr[n];
+}
 int main(){
     int n;
-    int max = 0;
-    int P[16] = {0};
-    int T[16] = {0};
-    scanf("%d",&n);
-    for(int i = 1; i <= n; i++){
-        scanf("%d %d", &T[i], &P[i]);
-    }
-    for(int i = 1; i <= n; i++){
-        int k = i;
-        int count = 0;
-        while(1){
-            if(k > n) break;
-            if(k == n && T[k] != 1 && k+T[k] > n) break;
-            count += P[k];
-            k += T[k];
-        }
-        max = count > max ? count : max;
-    }
-    printf("%d",max);
+    scanf("%d", &n);
+    fibo(n);
+    printf("%d", fibo(n));
 }
-
-// 4번 케이스에 있어서  5 4 3 2 1 1(2) 2(2) 3(2) 4(2) 5(2)로 들어가는데
-// 5 1(2) 2가 아닌 5 1(2) 3(2)이 되야 한다 -> 이경우를 잡아야 함
 
 //    clock_t start, finish;
 //    double duration;
